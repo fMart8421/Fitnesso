b57244bab8aa7abeaeec200be5ba7e906d008fbc
Fábio Martins <fabio.m@ua.pt>
2022-10-11 21:04:10 +0100

    created showcase element

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Showcase = () => {
+    return (
+        <section className="grid grid-cols-12 mx-10 gap-x-28">
+            <div className="col-span-6 justify-self-center">
+                <p className=" font-permanentmarker text-[#8ea5a0] text-6xl text-center -mb-3">
+                    Sandra Lyons
+                </p>
+                <div className="relative">
+                    <img
+                        className="rounded-xl h-[700px]"
+                        src="https://uploads-ssl.webflow.com/5e80894f63c557e083ed96b4/5e80a6e90c4ef4bcb4fd3071_Rectangle%20439.png"
+                        alt="woman doing exercise"
+                    />
+                    <button className="absolute bottom-10 left-10 aspect-square rounded-full border-2 border-primary bg-primary/30 h-[100px] flex items-center justify-center">
+                        <img
+                            className="max-w-[35%]"
+                            alt="arrow facing left"
+                            src="https://img.icons8.com/ios/50/FFFFFF/long-arrow-left.png"
+                        />
+                    </button>
+                    <button className="absolute bottom-10 right-10 aspect-square rounded-full border-2 border-primary bg-primary/30 h-[100px] flex items-center justify-center">
+                        <img
+                            className="max-w-[35%]"
+                            alt="arrow facing left"
+                            src="https://img.icons8.com/ios/50/FFFFFF/long-arrow-right.png"
+                        />
+                    </button>
+                </div>
+            </div>
+            <div className="col-span-6 mr-12 flex flex-col justify-center items-start">
+                <p className="font-medium">Hi, I'm Sandra Lyons</p>
+                <p className="text-7xl font-semibold mt-6">
+                    Your go to online fitness trainer and nutritionist.
+                </p>
+                <p className="text-xl my-8">
+                    After getting a bachelors in nutrition and fitness training
+                    I started working with people all around and changing their
+                    lives forever.
+                </p>
+                <button className="bg-primary text-white text-sm font-semibold p-5 pl-3 rounded-md">
+                    {"> "}
+                    <span className="pl-2">ONLINE COACHING</span>
+                </button>
+            </div>
+        </section>
+    );
+};
+
+export default Showcase;

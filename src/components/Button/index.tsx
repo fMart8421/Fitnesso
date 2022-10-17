5c15eab4067e4208ad3f0611df659f1012d1962c
Fábio Martins <fabio.m@ua.pt>
2022-10-17 19:17:40 +0100

    added button hover animation

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Button = (props: { text: string, className?: string, inlineTextClassName?:string }) => {
-    const className=`${props.className ? props.className :""} bg-primary text-white text-sm font-semibold p-5 pl-3 rounded-md`
+import classes from "./index.module.css";
+
+const Button = (props: {
+    text: string;
+    className?: string;
+    inlineTextClassName?: string;
+    overlayColor?:string
+}) => {
+    const [isHovering, setIsHovering] = useState(false);
+
+    const mouseEnterHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+        setIsHovering(true);
+    };
+    const mouseLeaveHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+        setIsHovering(false);
+    };
+
+    const className = `${
+        props.className ? props.className : ""
+    } bg-primary text-white text-sm font-semibold p-5 pl-3 rounded-md hover:-translate-y-1 transition ${
+        classes["group"]
+    } relative z-0`;
     return (
-        <button className={className}>
+        <button
+            className={className}
+            onMouseEnter={mouseEnterHandler}
+            onMouseLeave={mouseLeaveHandler}
+        >
+            <span
+                className={`inline-block ${
+                    isHovering ? classes["open"] : classes["close"]
+                } transition origin-right hover:white-filter`}
+            ></span>
+            <span
+                className={`absolute top-0 left-0 w-full h-full z-10 transition ${
+                    isHovering && (props.overlayColor ? props.overlayColor : "bg-white/40")
+                } duration-200`}
+            ></span>
             {"> "}
-            <span className={props.inlineTextClassName? props.inlineTextClassName : "pl-2"} style={{letterSpacing:"2px"}}>{props.text.toUpperCase()}</span>
+            <span
+                className={
+                    props.inlineTextClassName
+                        ? props.inlineTextClassName
+                        : "pl-2"
+                }
+                style={{ letterSpacing: "2px" }}
+            >
+                {props.text.toUpperCase()}
+            </span>
         </button>
     );
 };

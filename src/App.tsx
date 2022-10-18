0ef515fcd8b5f396a98b1ab0c2757cda9596efdf
Fábio Martins <fabio.m@ua.pt>
2022-10-18 19:01:37 +0100

    added scroll listener for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import React, { Fragment } from "react";
+import React, {
+    Fragment,
+    useCallback,
+    useEffect,
+    useRef,
+    useState,
+} from "react";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -9,19 +15,48 @@ import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 
 function App() {
+    const windowHeight = window.innerHeight;
+    const [percentage, setPercentage] = useState(1);
+    const mainRef = useRef<HTMLDivElement>(null);
+
+    const getTransformPercentage = useCallback(() => {
+        const mainY = mainRef.current?.getBoundingClientRect().y;
+        if (mainY && mainY >= -windowHeight / 2 && mainY < 0) {
+            console.log((mainY + windowHeight / 2) / (windowHeight / 2));
+            const _percentage = (mainY + windowHeight / 2) / (windowHeight / 2);
+            if (_percentage > 0.93) {
+                setPercentage(1);
+                return;
+            }
+            if (_percentage < 0.07) {
+                setPercentage(0);
+                return;
+            }
+            setPercentage(_percentage);
+        }
+    }, [windowHeight]);
+    useEffect(() => {
+        window.addEventListener("scroll", getTransformPercentage);
+    }, [getTransformPercentage]);
     return (
         <Fragment>
-            <Navbar />
-            <main>
+            <Navbar percentage={percentage} />
+            <main ref={mainRef}>
                 <Routes>
                     <Route path="/" element={<Home></Home>}></Route>
                     <Route path="/about" element={<About></About>}></Route>
-                    <Route path="/contact" element={<Contact></Contact>}></Route>
-                    <Route path="/free-workouts" element={<FreeWorkouts></FreeWorkouts>}></Route>
+                    <Route
+                        path="/contact"
+                        element={<Contact></Contact>}
+                    ></Route>
+                    <Route
+                        path="/free-workouts"
+                        element={<FreeWorkouts></FreeWorkouts>}
+                    ></Route>
                     <Route path="/shop" element={<Shop></Shop>}></Route>
                 </Routes>
             </main>
-            <Footer/>
+            <Footer />
         </Fragment>
     );
 }
